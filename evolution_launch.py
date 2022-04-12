3fc065ac2a301e770a91d1aa5c4aea80b9cf560d
KYC127 <48679612+KYE2138@users.noreply.github.com>
2022-04-12 22:31:29 +0800

    Update evolution_launch.py

--- evolution_launch.py
+++ evolution_launch.py
@@ -2,6 +2,7 @@ import os
 import time
 import argparse
 
+'''
 # TODO please configure TORCH_HOME and data_paths before running
 TORCH_HOME = "/ssd1/chenwy"  # Path that contains the nas-bench-201 database. If you only want to run on NASNET (i.e. DARTS) search space, then just leave it empty
 data_paths = {
@@ -9,6 +10,16 @@ data_paths = {
     "cifar100": "/ssd1/cifar.python",
     "ImageNet16-120": "/ssd1/ImageNet16",
     "imagenet-1k": "/ssd1/chenwy/imagenet_final",
+}
+'''
+
+# TODO please configure TORCH_HOME and data_paths before running
+TORCH_HOME = "/storage/dataset"  # Path that contains the nas-bench-201 database. If you only want to run on NASNET (i.e. DARTS) search space, then just leave it empty
+data_paths = {
+    "cifar10": "/storage/cifar10/cifar-10-batches-py",
+    "cifar100": "/storage/cifar100/cifar-100-python",
+    "ImageNet16-120": "/storage/ImageNet16",
+    "imagenet-1k": "/storage/imagenet_final",
 }
 
 total_steps = 1000
@@ -34,15 +45,14 @@ elif args.space == "darts":
 timestamp = "{:}".format(time.strftime('%h-%d-%C_%H-%M-%s', time.gmtime(time.time())))
 
 
-core_cmd = "CUDA_VISIBLE_DEVICES={gpuid} OMP_NUM_THREADS=4 python ./R_EA.py \
+core_cmd = "CUDA_VISIBLE_DEVICES={gpuid} OMP_NUM_THREADS=4 python3 ./R_EA.py \
 --save_dir {save_dir} --max_nodes {max_nodes} \
 --dataset {dataset} \
 --data_path {data_path} \
 --search_space_name {space} \
 --super_type {super_type} \
 --arch_nas_dataset {TORCH_HOME}/NAS-Bench-201-v1_0-e61699.pth \
---track_running_stats 1 \
---workers 0 --rand_seed {seed} \
+--workers 2 --rand_seed {seed} \
 --total_steps {total_steps} \
 --ea_population {ea_population_size} --ea_sample_size {ea_sample_size} \
 --timestamp {timestamp} \

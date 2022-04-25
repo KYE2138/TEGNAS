befe0de667b95694019ca0b1becc8f6f04f13524
KYC127 <ibiu1222@gmail.com>
2022-04-26 02:17:41 +0800

    Update test_ntk.py

--- lib/procedures/test_ntk.py
+++ lib/procedures/test_ntk.py
@@ -170,24 +170,24 @@ def get_ntk_n(loader, networks, loader_val=None, train_mode=False, num_batch=-1,
 
 # parameter
 loader = []
-cifar_train_input = torch.rand(64, 32, 32, 3)
+cifar_train_input = torch.rand(1, 32, 32, 3)
 cifar_train_target = torch.tensor([6])
 loader.append((cifar_train_input,cifar_train_target))
-cifar_train_input = torch.rand(64, 32, 32, 3)
+cifar_train_input = torch.rand(1, 32, 32, 3)
 cifar_train_target = torch.tensor([6])
 loader.append((cifar_train_input,cifar_train_target))
-cifar_train_input = torch.rand(64, 32, 32, 3)
+cifar_train_input = torch.rand(1, 32, 32, 3)
 cifar_train_target = torch.tensor([6])
 loader.append((cifar_train_input,cifar_train_target))
 
 loader_val = []
-cifar_train_input = torch.rand(64, 32, 32, 3)
+cifar_train_input = torch.rand(1, 32, 32, 3)
 cifar_train_target = torch.tensor([6])
 loader.append((cifar_train_input,cifar_train_target))
-cifar_train_input = torch.rand(64, 32, 32, 3)
+cifar_train_input = torch.rand(1, 32, 32, 3)
 cifar_train_target = torch.tensor([6])
 loader.append((cifar_train_input,cifar_train_target))
-cifar_train_input = torch.rand(64, 32, 32, 3)
+cifar_train_input = torch.rand(1, 32, 32, 3)
 cifar_train_target = torch.tensor([6])
 loader.append((cifar_train_input,cifar_train_target))
 

cc292285de0b9a905ab6dd205d8c9bfe94ad0daf
KYC127 <ibiu1222@gmail.com>
2022-04-26 03:15:03 +0800

    Update test_ntk.py

--- lib/procedures/test_ntk.py
+++ lib/procedures/test_ntk.py
@@ -170,13 +170,13 @@ def get_ntk_n(loader, networks, loader_val=None, train_mode=False, num_batch=-1,
 # parameter
 loader = []
 cifar_train_input = torch.randint(0, 255, (64, 32, 32, 3)).float()
-cifar_train_target = torch.randint(0, 9, (1,)).float()
+cifar_train_target = torch.randint(0, 9, (1,))
 loader.append((cifar_train_input,cifar_train_target))
 cifar_train_input = torch.randint(0, 255, (64, 32, 32, 3)).float()
-cifar_train_target = torch.randint(0, 9, (1,)).float()
+cifar_train_target = torch.randint(0, 9, (1,))
 loader.append((cifar_train_input,cifar_train_target))
 cifar_train_input = torch.randint(0, 255, (64, 32, 32, 3)).float()
-cifar_train_target = torch.randint(0, 9, (1,)).float()
+cifar_train_target = torch.randint(0, 9, (1,))
 loader.append((cifar_train_input,cifar_train_target))
 
 
@@ -186,10 +186,10 @@ cifar_train_input = torch.randint(0, 255, (64, 32, 32, 3)).float()
 cifar_train_target = torch.randint(0, 9, (1,)).float()
 loader_val.append((cifar_train_input,cifar_train_target))
 cifar_train_input = torch.randint(0, 255, (64, 32, 32, 3)).float()
-cifar_train_target = torch.randint(0, 9, (1,)).float()
+cifar_train_target = torch.randint(0, 9, (1,))
 loader_val.append((cifar_train_input,cifar_train_target))
 cifar_train_input = torch.randint(0, 255, (64, 32, 32, 3)).float()
-cifar_train_target = torch.randint(0, 9, (1,)).float()
+cifar_train_target = torch.randint(0, 9, (1,))
 loader_val.append((cifar_train_input,cifar_train_target))
 
 networks = []

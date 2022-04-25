b1990579099310618a37bba5e801774a4b36ca84
KYC127 <ibiu1222@gmail.com>
2022-04-25 16:32:45 +0800

    Update test_ntk.py

--- lib/procedures/test_ntk.py
+++ lib/procedures/test_ntk.py
@@ -149,9 +149,9 @@ loader = []
 cifar_train_input = torch.rand(1, 32, 32, 3)
 cifar_train_target = torch.tensor([6])
 loader.append((cifar_train_input,cifar_train_target))
-'''cifar_train_input = torch.rand(1, 32, 32, 3)
+cifar_train_input = torch.rand(1, 32, 32, 3)
 cifar_train_target = torch.tensor([6])
-loader.append((cifar_train_input,cifar_train_target))'''
+loader.append((cifar_train_input,cifar_train_target))
 '''cifar_train_input = torch.rand(1, 32, 32, 3)
 cifar_train_target = torch.tensor([6])
 loader.append((cifar_train_input,cifar_train_target))'''

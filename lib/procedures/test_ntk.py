c0151919397be6b1b9553fd308327962de41ffd8
KYC127 <ibiu1222@gmail.com>
2022-04-26 03:16:24 +0800

    Update test_ntk.py

--- lib/procedures/test_ntk.py
+++ lib/procedures/test_ntk.py
@@ -171,7 +171,6 @@ def get_ntk_n(loader, networks, loader_val=None, train_mode=False, num_batch=-1,
 loader = []
 cifar_train_input = torch.randint(0, 255, (64, 32, 32, 3)).float()
 cifar_train_target = torch.randint(0, 9, (1,))
-print (torch.randint(0, 9, (1,)))
 loader.append((cifar_train_input,cifar_train_target))
 cifar_train_input = torch.randint(0, 255, (64, 32, 32, 3)).float()
 cifar_train_target = torch.randint(0, 9, (1,))
@@ -184,7 +183,7 @@ loader.append((cifar_train_input,cifar_train_target))
 
 loader_val = []
 cifar_train_input = torch.randint(0, 255, (64, 32, 32, 3)).float()
-cifar_train_target = torch.randint(0, 9, (1,)).float()
+cifar_train_target = torch.randint(0, 9, (1,))
 loader_val.append((cifar_train_input,cifar_train_target))
 cifar_train_input = torch.randint(0, 255, (64, 32, 32, 3)).float()
 cifar_train_target = torch.randint(0, 9, (1,))

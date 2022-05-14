b793b9dd5816357f368a2d6c3a3220c87fed3f9d
KYC127 <ibiu1222@gmail.com>
2022-05-14 12:24:00 +0800

    Update test_ntk.py

--- lib/procedures/test_ntk.py
+++ lib/procedures/test_ntk.py
@@ -221,7 +221,27 @@ torch_model = convert_keras_model_to_torch_model(1)
 init_model(torch_model, xargs_init)
 networks.append(torch_model)
 
+torch_model = convert_keras_model_to_torch_model(100)
+init_model(torch_model, xargs_init)
+networks.append(torch_model)
+
+torch_model = convert_keras_model_to_torch_model(200)
+init_model(torch_model, xargs_init)
+networks.append(torch_model)
+
+torch_model = convert_keras_model_to_torch_model(300)
+init_model(torch_model, xargs_init)
+networks.append(torch_model)
+
+torch_model = convert_keras_model_to_torch_model(400)
+init_model(torch_model, xargs_init)
+networks.append(torch_model)
+
 torch_model = convert_keras_model_to_torch_model(500)
+init_model(torch_model, xargs_init)
+networks.append(torch_model)
+
+torch_model = convert_keras_model_to_torch_model(600)
 init_model(torch_model, xargs_init)
 networks.append(torch_model)
 
@@ -229,6 +249,8 @@ torch_model = convert_keras_model_to_torch_model(700)
 init_model(torch_model, xargs_init)
 networks.append(torch_model)
 
+#ntks = get_ntk_n(self._ntk_input_data, self._networks, train_mode=True, num_batch=1, num_classes=self._class_num)
+#num_batch = 1
 ntks, mses = get_ntk_n(loader, networks, loader_val=loader_val, train_mode=True, num_batch=1, num_classes=10)
 #ntks, mses = get_ntk_n(loader, networks, loader_val=loader_val, train_mode=True, num_batch=1, num_classes=num_classes)
 print ("ntks:",ntks)
